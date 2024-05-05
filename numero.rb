52ac921d51b034c62db12977834e8a2417c2c443
nicodemosmello <niocdemusgimello21@gmail.com>
2024-05-04 22:10:06 -0300

    atualização no arquivo numero.br

---
+++ numero.rb
@@ -0,0 +1,21 @@
+#Desenvolva um sistema de cálculos onde o usuário possa informar dois números e retorne para ele um resultado
+
+#Utilize as 4 operações matemáticas
+
+#Informe as variáveis x e y e a operação utilizada
+puts "informe o primeiro número:"
+x = gets.chomp.to_f
+puts "Informe o segundo número:"
+y = gets.chomp.to_f
+
+
+puts "Soma:"
+puts x + y
+puts "Subt:"
+puts x - y
+puts "Mult:"
+puts x * y
+puts "Div:"
+puts x / y
+
+#Informa o resusltado
